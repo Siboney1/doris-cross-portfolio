9183bf4e7ba1fde7293f3ec86274953fbffcc8e5
Siboney <38500301+Siboney1@users.noreply.github.com>
2026-04-08 14:12:24 -0700

    Remove unused imports from _app.js
    Remove unused imports from _app.js

---
+++ _app.js
@@ -0,0 +1,22 @@
+
+const GA_ID = 'G-DR1CZVNR13';
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_ID}');
+                `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    );
+}
